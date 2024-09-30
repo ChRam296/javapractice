b655fd847839bf964811886c0db80029ce8b0204
ChRam296 <ramch296@gmail.com>
2024-09-30 11:21:06 +0530

    Java practise

--- src/Test1.java
+++ src/Test1.java
@@ -1,9 +1,15 @@
 
 public class Test1 {
+	int a = 10;
+	int b = 20;
+	
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+    //int c = a+b;
+		Test1 t1 = new Test1();
+		int c = t1.a + t1.b;
+		System.out.println(c);
 	}
 
 }
